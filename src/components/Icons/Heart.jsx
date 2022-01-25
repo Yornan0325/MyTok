78667545cd519feb8143cecb36fbb11f43857a58
Yornan0325 <jhornan.coningenieria@gmail.com>
2022-01-24 20:43:08 -0500

    icono share

--- src/components/Icons/Heart.jsx
+++ src/components/Icons/Heart.jsx
@@ -3,7 +3,7 @@ export const Heart = ({ fill }) => (
     width='56'
     height='56'
     viewBox='0 0 24 24'
-    fill='none'
+    fill='#fff'
   >
     <path
       fillRule='evenodd'
@@ -13,4 +13,13 @@ export const Heart = ({ fill }) => (
     />
 
   </svg>
+  // <svg
+  //   width='56'
+  //   height={56}
+  //   viewBox='8 8 40 40'
+  //   fill='#fff'
+  //   xmlns='http://www.w3.org/2000/svg'
+  // >
+  //   <path fill-rule='evenodd' clip-rule='evenodd' d='M7.5 2.25C10.5 2.25 12 4.25 12 4.25C12 4.25 13.5 2.25 16.5 2.25C20 2.25 22.5 4.99999 22.5 8.5C22.5 12.5 19.2311 16.0657 16.25 18.75C14.4095 20.4072 13 21.5 12 21.5C11 21.5 9.55051 20.3989 7.75 18.75C4.81949 16.0662 1.5 12.5 1.5 8.5C1.5 4.99999 4 2.25 7.5 2.25Z' />
+  // </svg>
 )
