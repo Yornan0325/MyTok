4563154b062bc0404e242207fff931a1626ef1ba
Yornan0325 <jhornan.coningenieria@gmail.com>
2022-01-24 20:44:50 -0500

    importar los componentes- hare-heart-comment

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,17 +1,22 @@
 import { Heart } from '../Icons/Heart'
+import { Share } from '../Icons/Share'
+import { Comment } from '../Icons/Comment'
 import styles from './styles.module.css'
 
-export default function VideoPlayerAtions () {
+export default function VideoPlayerAtions ({ likes = 172, comments = 112, shares = 142, hearted = false }) {
   return (
     <aside className={styles.actions}>
       <div className={styles.action}>
         <Heart />
+        <strong title='like'>{likes}</strong>
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Share />
+        <strong title='shares'>{shares}</strong>
       </div>
       <div className={styles.action}>
-        <Heart />
+        <Comment />
+        <strong title='comments'>{comments}</strong>
       </div>
     </aside>
   )
